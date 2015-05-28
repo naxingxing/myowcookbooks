8bc7c595c306871c99f224f9dbdb0df4ba827804
rexter <rextechnical@gmail.com>
2015-05-28 23:53:11 +0800

    bug fix in the undeploy script

--- playwebserver/files/default/undeployhyyq.sh
+++ playwebserver/files/default/undeployhyyq.sh
@@ -4,7 +4,7 @@
 kill -9 `pgrep java`
 
 rm -r /home/ubuntu/deployment/hyyqsite-1.0-SNAPSHOT
-rm /home/ubuntu/download/hyyqsite-1.0-SNAPSHOT.zip
+rm /home/ubuntu/deployment/hyyqsite-1.0-SNAPSHOT.zip
 
 
 
