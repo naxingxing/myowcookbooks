f8bc95006907ffe5296465b091e83c8da0398ce7
rexter <rextechnical@gmail.com>
2015-05-31 10:11:41 +0800

    test code

--- playwebserver/recipes/deploy.rb
+++ playwebserver/recipes/deploy.rb
@@ -16,36 +16,34 @@ unless node[:project].nil?
       if app[:name] == node[:project][:name]
         log "\n\n - hello world!!!\n\n"
         pname = node[:project][:name]
-        rname = node[:deploy]["#{pname}"][:scm][:repository]
-        log "pname is " + pname + " and test - rname is " + rname
+        repourl = node[:deploy]["#{pname}"][:scm][:repository]
+        
+        =begin
+        unless node[:deploy]['hyyqsite'].nil?
+          git "#{node[:deploy]['hyyqsite'][:scm][:repository]}" do
+            repository node[:deploy]['hyyqsite'][:scm][:repository]
+            action :sync
+            user "ubuntu"
+            group "ubuntu"
+            destination "/home/ubuntu/download/hyyqsite"
+          end
+        end
+
+        cookbook_file "/tmp/deploy_dist" do
+          source "deployhyyq.sh"
+          mode '0755'
+          user 'ubuntu'
+        end
+
+        execute "run_deploy" do
+          user "ubuntu"
+          cwd "/tmp"
+          command "./deploy_dist"
+        end
+        =end
+        
       end
     end
   end
-else
-node[:opsworks][:applications].each do |app|
-  log "\n\n\n-app name: #{app[:name]}\n-application_type: #{app[:application_type]}\n-slug_name: #{app[:slug_name]}\n\n\n"
-end
-
-unless node[:deploy]['hyyqsite'].nil?
-  git "#{node[:deploy]['hyyqsite'][:scm][:repository]}" do
-    repository node[:deploy]['hyyqsite'][:scm][:repository]
-    action :sync
-    user "ubuntu"
-    group "ubuntu"
-    destination "/home/ubuntu/download/hyyqsite"
-  end
-end
-
-cookbook_file "/tmp/deploy_dist" do
-  source "deployhyyq.sh"
-  mode '0755'
-  user 'ubuntu'
-end
-
-execute "run_deploy" do
-  user "ubuntu"
-  cwd "/tmp"
-  command "./deploy_dist"
-end
 end
 log "\n\n==================================== playwebserver::deploy - END ===================================== \n"
