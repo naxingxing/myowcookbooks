edd326c5b32d1e8110877c4db925eb9acbda37f9
rexter <rextechnical@gmail.com>
2015-05-31 20:04:03 +0800

    trying to fix a deploy bug

--- playwebserver/files/default/deployhyyqsite.sh
+++ playwebserver/files/default/deployhyyqsite.sh
@@ -4,16 +4,7 @@
 
 
 
-cd /home/ubuntu/download/hyyqsite;
-mkdir ranranran
-activator dist;
-mkdir endendend
-cp target/universal/hyyqsite-1.0-SNAPSHOT.zip /home/ubuntu/deployment/;
-cd /home/ubuntu/deployment && unzip hyyqsite-1.0-SNAPSHOT.zip;
-
-
-
-nohup ./hyyqsite-1.0-SNAPSHOT/bin/hyyqsite &
+cd /home/ubuntu/download/hyyqsite && mkdir ranranran1 && activator dist && mkdir endendend1 && cp target/universal/hyyqsite-1.0-SNAPSHOT.zip /home/ubuntu/deployment/ && cd /home/ubuntu/deployment && unzip hyyqsite-1.0-SNAPSHOT.zip && nohup ./hyyqsite-1.0-SNAPSHOT/bin/hyyqsite &
 
 
 
