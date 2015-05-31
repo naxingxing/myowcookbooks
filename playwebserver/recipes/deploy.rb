3cea8c54987c21e05637973756bac1b767ea6f78
rexter <rextechnical@gmail.com>
2015-05-31 11:43:34 +0800

    test code

--- playwebserver/recipes/deploy.rb
+++ playwebserver/recipes/deploy.rb
@@ -10,37 +10,18 @@ log "\n\n=================================== playwebserver::deploy - RUNNING ===
 unless node[:project].nil?
   log "\n\n\n\n\nThis is awesome!!! \n #{node[:project][:name]} - #{node[:project][:domain]}\n\n\n\n"
   #log "\n\n#{node[:deploy][node[:project][:name]][:scm][:repository]} - good test 1\n\n"
-  unless node[:opsworks][:applications].nil?
-    node[:opsworks][:applications].each do |app|
+  apps = node[:opsworks][:applications]
+  unless apps.nil?
+    apps.each do |app|
       log "#{app[:name]} #{app[:application_type]} good test"
       if app[:name] == node[:project][:name]
         log "\n\n - hello world!!!\n\n"
         pname = node[:project][:name]
         repourl = node[:deploy]["#{pname}"][:scm][:repository]
         
-        =begin
-        unless node[:deploy]['hyyqsite'].nil?
-          git "#{node[:deploy]['hyyqsite'][:scm][:repository]}" do
-            repository node[:deploy]['hyyqsite'][:scm][:repository]
-            action :sync
-            user "ubuntu"
-            group "ubuntu"
-            destination "/home/ubuntu/download/hyyqsite"
-          end
-        end
+        
 
-        cookbook_file "/tmp/deploy_dist" do
-          source "deployhyyq.sh"
-          mode '0755'
-          user 'ubuntu'
-        end
-
-        execute "run_deploy" do
-          user "ubuntu"
-          cwd "/tmp"
-          command "./deploy_dist"
-        end
-        =end
+ 
         
       end
     end
