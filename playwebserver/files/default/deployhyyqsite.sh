3e9721068a52a6108b9e0b1ba07f0b7322495dcf
rexter <rextechnical@gmail.com>
2015-05-31 18:39:35 +0800

    fix deploy bug

--- playwebserver/files/default/deployhyyqsite.sh
+++ playwebserver/files/default/deployhyyqsite.sh
@@ -2,28 +2,12 @@
 
 
 
-#LINK_OR_DIR='/home/ubuntu/download/hyyqsite'
-
-#if [ -d "$LINK_OR_DIR" ]; then 
-#  if [ -L "$LINK_OR_DIR" ]; then
-    # It is a symlink!
-    # Symbolic link specific commands go here.
-#    rm "$LINK_OR_DIR"
-#	cd /home/ubuntu/download && git clone https://github.com/naxingxing/hyyqsite.git
-#  else
-    # It's a directory!
-    # Directory command goes here.
-#    cd "$LINK_OR_DIR" && git pull
-#  fi
-#else
-#	cd /home/ubuntu/download && git clone https://github.com/naxingxing/hyyqsite.git
-#fi
 
 
-
-cd /home/ubuntu/download/hyyqsite && activator dist
-cp target/universal/hyyqsite-1.0-SNAPSHOT.zip /home/ubuntu/deployment/
-cd /home/ubuntu/deployment && unzip hyyqsite-1.0-SNAPSHOT.zip
+cd /home/ubuntu/download/hyyqsite;
+activator dist;
+cp target/universal/hyyqsite-1.0-SNAPSHOT.zip /home/ubuntu/deployment/;
+cd /home/ubuntu/deployment && unzip hyyqsite-1.0-SNAPSHOT.zip;
 
 
 
