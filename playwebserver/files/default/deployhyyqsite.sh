990c42c73449999ddb2bd59948a5503681d416f3
rexter <rextechnical@gmail.com>
2015-05-31 17:43:50 +0800

    debug point

--- playwebserver/files/default/deployhyyqsite.sh
+++ playwebserver/files/default/deployhyyqsite.sh
@@ -19,12 +19,13 @@
 #	cd /home/ubuntu/download && git clone https://github.com/naxingxing/hyyqsite.git
 #fi
 
+mkdir beginning
 
 cd /home/ubuntu/download/hyyqsite && activator dist
 cp target/universal/hyyqsite-1.0-SNAPSHOT.zip /home/ubuntu/deployment/
 cd /home/ubuntu/deployment && unzip hyyqsite-1.0-SNAPSHOT.zip
 
-
+mkdir ending
 
 nohup ./hyyqsite-1.0-SNAPSHOT/bin/hyyqsite &
 
