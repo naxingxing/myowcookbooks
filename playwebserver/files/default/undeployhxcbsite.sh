f0af4d4badd8418d038ad25c7cf7e27633009895
rexter <rextechnical@gmail.com>
2015-05-31 18:15:57 +0800

    fix deploy bug

--- playwebserver/files/default/undeployhxcbsite.sh
+++ playwebserver/files/default/undeployhxcbsite.sh
@@ -3,7 +3,7 @@
 
 kill -9 `pgrep java`
 
-rm -r /home/ubuntu/deployment/hxcbsite-*-SNAPSHOT
+rm -rf /home/ubuntu/deployment/hxcbsite-*-SNAPSHOT
 rm /home/ubuntu/deployment/hxcbsite-*-SNAPSHOT.zip
 
 
--- playwebserver/files/default/undeployhyyqsite.sh
+++ playwebserver/files/default/undeployhyyqsite.sh
@@ -3,7 +3,7 @@
 
 kill -9 `pgrep java`
 
-rm -r /home/ubuntu/deployment/hyyqsite-*-SNAPSHOT
+rm -rf /home/ubuntu/deployment/hyyqsite-*-SNAPSHOT
 rm /home/ubuntu/deployment/hyyqsite-*-SNAPSHOT.zip
 
 
