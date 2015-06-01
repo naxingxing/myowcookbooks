c5237b6d6a8abaedb3c89154434b22f0bffb113e
rexter <rextechnical@gmail.com>
2015-06-01 18:28:13 +0800

    testing

--- playwebserver/files/default/deployhyyqsite.sh
+++ playwebserver/files/default/deployhyyqsite.sh
@@ -6,9 +6,11 @@
 
 cd /home/ubuntu/download/hyyqsite
 (mkdir line2_st && activator dist && mkdir line2_ed);
+wait
 (mkdir line3_st && cp target/universal/hyyqsite-1.0-SNAPSHOT.zip /home/ubuntu/deployment/ && mkdir line3_ed);
+wait
 (mkdir line4_st && cd /home/ubuntu/deployment && unzip hyyqsite-1.0-SNAPSHOT.zip && mkdir line4_ed);
-
+wait
 
 
 nohup ./hyyqsite-1.0-SNAPSHOT/bin/hyyqsite &
