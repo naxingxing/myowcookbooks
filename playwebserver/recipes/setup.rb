f28f6de7fbc39db5e4eb5cb0d3c90cc6bd1a82ad
rexter <rextechnical@gmail.com>
2015-06-01 21:00:54 +0800

    testing

--- playwebserver/recipes/setup.rb
+++ playwebserver/recipes/setup.rb
@@ -74,8 +74,8 @@ script "install_sbt" do
   code <<-EOH
     #insert bash script
     echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-    sudo apt-get update
-    sudo apt-get install sbt
+    sudo apt-get -y update
+    sudo apt-get -y install sbt
   EOH
 end
 
